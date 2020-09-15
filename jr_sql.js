bffa2f501a33d66127e1e8bb3c4f9305ce62d89f
0red <0red@users.noreply.github.com>
2020-09-15 22:17:21 +0200

    Update jr_sql.js

--- jr_sql.js
+++ jr_sql.js
@@ -1,18 +1,70 @@
-async function sql(q,bulk_insert=false) {
+/**
+ * run sqlite query via jr_sqlite_secure.php
+ * q should be less then 4,294,967,000 bytes !!!
+ * @param {*} q 
+ * @param {*} bulk_insert 
+ */
+async function sql(q,bulk_insert=false,noout=false) {
+  console.log(['sql',q])
+  const maxint1=300000000; //4294960000
+  if (isArray(q)) {
+    let q1=[];let dl=0;let w=false;
+    for (let x1 of q) {
+      //console.log([dl,x1.length,dl+x1.length,maxint1,x1])
+      if (dl && x1.length+dl>maxint1) {
+        w=await sql(q1.join(" "),bulk_insert);
+        q1=[];
+        dl=0;
+      }
+      q1.push(x1);
+      dl+=x1.length;
+    };
+    if (q1.length) w=await sql(q1.join(" "),bulk_insert);
+    return w;
+  }
+
+  if (!(isString(q))) {
+    console.warn([typeof q,q])
+    throw ("SQL command should be String");
+  }
+  if (!q.trim()) throw ("Empty SQL command. "+q);
+  
+
   if (bulk_insert) q=[
       "PRAGMA synchronous = OFF;",
       "PRAGMA locking_mode = EXCLUSIVE;",
       "PRAGMA journal_mode = OFF;",
       "PRAGMA temp_store = MEMORY;",
-      "PRAGMA cache_size = 500000;",
+      "PRAGMA cache_size = 50000000;",
       "PRAGMA count_changes = OFF;",
       "BEGIN TRANSACTION;",
       q,";COMMIT;"].join("\n");
-  let x=await ajax_post("jr_sqlite_secure.php",{qjr:q},'json');;
+  let x=await ajax_post("jr_sqlite_secure.php",{qjr:q,qo:noout?1:0},'json');;
   if (x.out) x.out=x.out.split("\n");
   console.log([q,x]);
   return x;
 }
+
+
+const ajax_post = (url,data,typ) => new Promise ( (resolve, reject) => {
+      $.ajax({
+         method:"POST",
+         url:url,
+         data:data || {},
+         dataType: typ || 'text',
+         success: (dane,status,xhr) => resolve(dane,status,xhr),
+         error: (xhr,status,errorText) => reject(xhr,status,errorText)
+      })
+   }
+  );
+
+const isString = a => (!!a) && (a.constructor === String);
+const isNumber = a => (!!a) && (a.constructor === Number);
+const isArray = a => (!!a) && (a.constructor === Array);
+
+
+// ---------------------------- other functions --- not tested YET !!!
+
 
 async function sql_key_set(key,v,create_table=false) {
   if (!isString(key)) return false;
@@ -61,5 +113,4 @@ function str_uncompress(binaryString,double_chr=false) {
     return (pako && pako.inflate) ? JSON.parse(pako.inflate(binaryString, { to: 'string' })) : binaryString;
 }
 
-const isString = a => (!!a) && (a.constructor === String);
-const isNumber = a => (!!a) && (a.constructor === Number);
+
